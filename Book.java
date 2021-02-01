a2e325cea84c28b72be968c477a1de1c055cd647
600020344 <77069279+600020344@users.noreply.github.com>
2021-02-01 11:06:11 -0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,10 +1,11 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("ISBN:  096548534X is valid "+Book.isValidISBN("096548534X"));
+public class Book
+{
 
-    System.out.println("ISBN:  12387658X is valid "+Book.isValidISBN("12387658X"));
-    
-    System.out.println("ISBN:  0328931829 is valid "+Book.isValidISBN("0328931829"));
+  public static boolean isValidISBN(String isbn)
+  {
+
+    //add your code here
+
 
   }
-}
+} 
